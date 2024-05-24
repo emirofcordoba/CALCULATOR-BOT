87b7f2b668181aaad891969392ed70e9fe84649f
Calinion (Nerd) <159137813+emirofcordoba@users.noreply.github.com>
2024-05-24 20:17:51 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+![MogaCalculator Bot Logo](https://github.com/emirofcordoba/CALCULATOR-BOT/blob/main/logo.png)
+
+# **This project is made by [t.me/emirofcordoba](https://t.me/MogaTeam) alone**
+
+# **MogaCalculator Bot**
+
+## **This project is in Node.js, and its purpose is to help you create your own calculator Telegram bot**
+
+## **Installation Instructions**
+
+## **Steps to set up the project at Glitch:**
+
+<p align="center">
+  <a href="https://glitch.com/edit/#!/import/git?url=https://github.com/emirofcordoba/CALCULATOR-BOT.git">
+    <img src="https://img.shields.io/badge/Import%20To%20Glitch-Click%20to%20Import-brightgreen?style=for-the-badge" alt="Import to Glitch Button" width="820" />
+  </a>
+</p>
+
+1. **Step one** - Click the "**Import To Glitch**" button
+2. **Step two** - Wait some time for the project to be synced
+3. **Step three** - Go to the `.env` file in the project and input the proper variables
+4. **Step four** - Click on "**🔎 Preview**" in the top right corner at the bottom
+5. **Step five** - Click on the second option under preview 
+6. **Step six** - Done, now enjoy your own calculator bot🤩
+7. **Step seven** - If you want to make your Glitch project uptime then contact http://t.me/emirofcordoba
+## **Video Tutorial** ##
+## **[![Video Tutorial](https://github.com/emirofcordoba/CALCULATOR-BOT/blob/main/setup.png)](https://github.com/emirofcordoba/CALCULATOR-BOT/blob/main/setup.mp4?raw=true)**
+
+## **Social Contacts**
+
+<p align="center">
+  <a href="https://t.me/emirofcordoba">
+    <img src="https://img.shields.io/badge/CONTACT-TELEGRAM-blue?style=for-the-badge&logo=telegram" alt="Telegram Badge"/>
+  </a>
+  <a href="https://instagram.com/emirofcordoba">
+    <img src="https://img.shields.io/badge/CONTACT-INSTAGRAM-red?style=for-the-badge&logo=instagram" alt="Instagram Badge"/>
+  </a>
+  <a href="https://www.youtube.com/@highjacker_kidnapper/about">
+    <img src="https://img.shields.io/badge/CONTACT-YOUTUBE-red?style=for-the-badge&logo=youtube" alt="Youtube Badge"/>
+  </a>
+</p>
---
+++ server.js
@@ -0,0 +1,151 @@
+var fs = require('fs');
+var TelegramBot = require('node-telegram-bot-api');
+var mathjs = require('mathjs');
+var express = require('express');
+var app = express();
+var PORT = 5000;
+var bot = new TelegramBot(process.env.TOKEN, { polling: true });
+
+var CHANNEL_USERNAME = process.env.CHANNEL_USERNAME;
+
+var joinChannelButton = {
+   text: 'JOIN CHANNEL👻',
+   url: process.env.JOIN_CHANNEL_URL,
+};
+
+var joinedButton = {
+   text: 'JOINED🥁',
+   callback_data: 'check_joined'
+};
+
+async function sendJoinChannelMessage(chatId) {
+    var options = {
+        parse_mode: 'HTML',
+        reply_markup: {
+            inline_keyboard: [
+                [joinChannelButton, joinedButton]
+            ]
+        }
+    };
+    var message = "You can use this Telegram bot to upload any file and download that later🤩, but you're not joined to our channel. Please join and click 'Joined🙂'";
+    await bot.sendMessage(chatId, `<pre>${message}</pre>`, options);
+}
+
+bot.onText(/\/start/, async (msg) => {
+    var chatId = msg.chat.id;
+    var member = await bot.getChatMember(CHANNEL_USERNAME, chatId);
+
+    if (member.status === 'member' || member.status === 'administrator' || member.status === 'creator') {
+        await startCommand(chatId);
+    } else {
+        await sendJoinChannelMessage(chatId);
+    }
+});
+
+bot.on('callback_query', async (query) => {
+    var chatId = query.message.chat.id;
+
+    if (query.data === 'check_joined') {
+        var member = await bot.getChatMember(CHANNEL_USERNAME, chatId);
+
+        if (member.status === 'member' || member.status === 'administrator' || member.status === 'creator') {
+            await startCommand(chatId);
+        } else {
+            var randomMessage = "You didn't join all our communities, please join them first to use me perfectly 🥹";
+
+            await bot.answerCallbackQuery(query.id, {
+                text: randomMessage,
+                show_alert: true
+            });
+
+            await sendJoinChannelMessage(chatId);
+        }
+    }
+});
+
+bot.on('callback_query', async (query) => {
+    var chatId = query.message.chat.id;
+    var userId = query.from.id;
+    var data = query.data;
+
+    if (data === 'upload') {
+        var member = await bot.getChatMember(CHANNEL_USERNAME, userId);
+
+        if (member.status === 'member' || member.status === 'administrator' || member.status === 'creator') {
+            await bot.sendMessage(chatId, "Send or forward the file you wanna upload📂");
+        } else {
+            await sendJoinChannelMessage(chatId);
+        }
+    }
+});
+
+async function startCommand(chatId) {
+    var userName = ''; // You should define userName variable here
+    var message = `<pre>Hello ${userName}, I am a calculator bot. Use buttons or go inline in this chat or another chat. In inline mode, type numbers or signs like <b>1+1</b>, <b>3*3</b>, <b>3/2</b>, <b>4-2</b>, or combine multiple numbers or signs, e.g., <b>88+28-6</b>. Choose an option below:</pre>`;
+    var inlineKeyboard = [
+        [{ text: 'GO INLINE (HERE)🤖', switch_inline_query_current_chat: '' }],
+        [{ text: 'GO INLINE (ABROAD)👾', switch_inline_query: '' }],
+    ];
+    var options = {
+        parse_mode: 'HTML',
+        reply_markup: {
+            inline_keyboard: inlineKeyboard
+        }
+    };
+    await bot.sendMessage(chatId, `<pre>${message}</pre>`, options);
+}
+
+bot.on('inline_query', async (query) => {
+  var results = [];
+  var expression = query.query;
+
+  if (expression) {
+    try {
+      // Evaluate the expression using mathjs
+      var resultValue = mathjs.evaluate(expression);
+
+      // Create an inline query result
+      var result = {
+        type: 'article',
+        id: '1', // Unique ID for the result
+        title: `${expression} = ?`,
+        input_message_content: {
+          message_text: `${expression} = ?`, 
+        },
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: 'Show Result🧮', callback_data: `result_${expression}_${resultValue}` }
+            ]
+          ]
+        }
+      };
+
+      results.push(result);
+    } catch (error) {
+      console.error('Error evaluating expression:', error);
+      // Consider sending an error message to the user if needed
+    }
+  }
+
+  await bot.answerInlineQuery(query.id, results); 
+});
+
+bot.on('callback_query', async (callbackQuery) => {
+  var parts = callbackQuery.data.split('_');
+  var expression = parts[1];
+  var resultValue = parts[2];
+
+  await bot.answerCallbackQuery(callbackQuery.id, { 
+    text: `Result🧮: ${resultValue}`, 
+    show_alert: true 
+  });
+});
+
+app.get('/', async (req, res) => {
+   res.send("Bot is up");
+ });
+ 
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+});
